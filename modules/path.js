48b3ab98c9da6c3ce0051d854a16c2291ea0fbc3
Gustavo <gustgouvea@hotmail.com>
2023-05-10 11:56:05 -0300

    estudo de path e fs

--- modules/path.js
+++ modules/path.js
@@ -1,4 +1,16 @@
 const path = require("path");
 
-// Basename
+// Apenas o nome do arquivo atual
 console.log(path.basename(__filename));
+
+// Nome do Diretório atual
+console.log(path.dirname(__filename));
+
+// Estensão do arquivo
+console.log(path.extname(__filename));
+
+// Criar objeto Path
+console.log(path.parse(__filename));
+
+// Juntar caminhos de arquivos
+console.log(path.join(__dirname, "teste", "test.html"));
