2cec399d08e045accc3e6fa0cfb252e1f3517e11
Gustavo <gustgouvea@hotmail.com>
2023-05-12 12:09:31 -0300

    estudos de modulo fs

--- modules/fs.js
+++ modules/fs.js
@@ -1,11 +1,37 @@
 const fs = require("fs");
 const path = require("path");
 
-//Criar uma pasta
-fs.mkdir(path.join(__dirname, "/test"), (error) => {
-  if (error) {
-    return console.log("Erro: ", error);
-  }
+// Criar uma pasta
+// fs.mkdir(path.join(__dirname, "/test"), (error) => {
+//  if (error) {
+//    return console.log("Erro: ", error);
+//  }
 
-  console.log("Pasta criada com sucesso!");
-});
+//  console.log("Pasta criada com sucesso!");
+// });
+
+// Criar um arquivo
+fs.writeFile(
+  path.join(__dirname, "/test", "text.txt"),
+  "hello node!",
+  (error) => {
+    if (error) {
+      return console.log("Erro: ", error);
+    }
+
+    console.log("Arquivo criado com sucesso!");
+  }
+);
+
+// Adicionar á um arquivo
+fs.appendFile(
+  path.join(__dirname, "/test", "test.txt"),
+  "hello world!",
+  (error) => {
+    if (error) {
+      return console.log("Error: ", error);
+    }
+
+    console.log("Arquivo modificado com sucesso!")
+  }
+)
